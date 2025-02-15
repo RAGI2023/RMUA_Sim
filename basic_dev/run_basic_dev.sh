06073ab350af03606ed58f1dc5766e4ab12ebfbf
RAGI <yinzhe23@bupt.edu.cn>
2025-02-15 23:52:24 +0800

    docker构建完成

--- basic_dev/run_basic_dev.sh
+++ basic_dev/run_basic_dev.sh
@@ -2,4 +2,9 @@
 # --name 指定名称
 # --rm 结束后自动释放资源
 
-docker run -it --net host --name basic_dev --rm  basic_dev
+# docker run -it --net host --name basic_dev --rm  basic_dev
+docker run --rm \
+  -e ROS_MASTER_URI=http://$(hostname -I | awk '{print $1}'):11311 \
+  -e ROS_IP=$(hostname -I | awk '{print $1}') \
+  --network host \
+  rmua
--- basic_dev/setup.bash
+++ basic_dev/setup.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /basic_dev
 source devel/setup.bash
-rosrun basic_dev basic_dev
+rostopic list
+# rosrun basic_dev keyboard_ctrl &
+# roslaunch yolov5_ros yolov5.launch 
